3a0192a81cc26e90de334a2d267ff75789c81cf3
IvanBarr <ivanbar.webdev@gmail.com>
2017-07-28 16:04:34 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,86 @@
-var imgArray = ["imgs/barcelona.png","imgs/clouds.png","imgs/blue-apple.png"];
-var i = 0;
-function next(){
-  var imgSlider = document.getElementById('imgSlider');
-  i++;
-  imgSlider[i];
-  if(i >= imgArray.length){
-    i = 0;
+function elementInViewport(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
   }
-  imgSlider.src = imgArray[i];
+
+  return (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    (top + height) <= (window.pageYOffset + window.innerHeight) &&
+    (left + width) <= (window.pageXOffset + window.innerWidth)
+  );
 }
 
-function prev(){
- var imgSlider = document.getElementById('imgSlider');
- i--;
- imgSlider[i];
- if(i < 0){
-   i = imgArray.length-1;
- }
- imgSlider.src = imgArray[i];
+
+var smartIcons = document.getElementsByClassName('circles');
+// Talented
+var profileWrapper = document.getElementsByClassName('profile-wrapper');
+var profile = document.getElementsByClassName('profiles');
+// Elegant section
+var pic = document.getElementsByClassName('pic');
+
+profile[0].style.left = '-15px';
+profile[1].style.left = '-30px';
+profile[2].style.right = '-30px';
+profile[3].style.right = '-15px';
+
+// List through profiles and pics and add opacity and transform
+for(var i = 0; i<profile.length && i<pic.length; i++){
+  profile[i].style.opacity = '0';
+  pic[i].style.transform = 'scale(.01)';
 }
+
+
+$(window).scroll(function(){
+  if(elementInViewport(smartIcons[0])){
+    smartIcons[0].style.opacity = 1;
+  }
+  if(elementInViewport(smartIcons[1])){
+    smartIcons[1].style.opacity = 1;
+  }
+  if(elementInViewport(smartIcons[2])){
+    smartIcons[2].style.opacity = 1;
+  }
+
+  // TEAM
+
+  // two Left profiles
+  if(elementInViewport(profileWrapper[0])){
+    profile[0].style.left = '0';
+    profile[0].style.opacity = '1';
+  }
+  if(elementInViewport(profileWrapper[1])){
+    profile[1].style.left = '0';
+    profile[1].style.opacity = '1';
+  }
+  // two right profiles
+  if(elementInViewport(profileWrapper[2])){
+    profile[2].style.right = '0';
+    profile[2].style.opacity = '1';
+  }
+  if(elementInViewport(profileWrapper[3])){
+    profile[3].style.right = '0';
+    profile[3].style.opacity = '1';
+  }
+
+  // Elegant
+  if(elementInViewport(pic[0])){
+    pic[0].style.transform = 'scale(1)';
+  }
+  if(elementInViewport(pic[1])){
+    pic[1].style.transform = 'scale(1)';
+  }
+  if(elementInViewport(pic[2])){
+    pic[2].style.transform = 'scale(1)';
+  }
+  if(elementInViewport(pic[3])){
+    pic[3].style.transform = 'scale(1)';
+  }
+});
